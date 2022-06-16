66ff8a2c4375463f1dc9a762a57b6f310b49d74f
Nicolai Heuck <n.heuck@hessel.dk>
2022-06-16 11:29:31 +0200

    Init

---
+++ src/Components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type ButtonProps = {
+    children: React.ReactNode
+}
+
+const Button = (props: ButtonProps) => {
+  return (
+    <button>{props.children}</button>
+  )
+}
+
+export default Button
